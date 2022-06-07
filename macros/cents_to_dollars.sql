e35846fa539285712f797a056d257c8ab904f877
Pablo Agustin Vieyra <104534435+AgustinVieyra@users.noreply.github.com>
2022-06-07 15:25:29 -0500

    Update cents_to_dollars.sql

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,3 +1,7 @@
 {% macro cents_to_dollars(column_name, decimal_places=2) -%}
     round( 1.0 * {{ column_name }} / 100, {{ decimal_places }} )
 {%- endmacro %}
+
+/*Currently used this macro in:
+    stg_payments.sql
+*/
